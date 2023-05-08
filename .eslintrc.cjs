95480f92d397439a563d937efe45471993d25b40
ndom91 <yo@ndo.dev>
2023-05-08 23:06:42 +0200

    fix: more initial setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,16 @@ module.exports = {
     'prettier'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ['@typescript-eslint'],
+  env: {
+    es2021: true
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
   root: true,
 };
